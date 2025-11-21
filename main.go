0a116f69c12384c9d0f1d97d0546711a9f0706df
Beijing-corn87 <111331826+bluekiwidev@users.noreply.github.com>
2025-11-22 09:32:01 +1300

    Refactor main.go: improve code readability by adding comments and using a constant for sleep time

--- main.go
+++ main.go
@@ -9,9 +9,12 @@ import (
 )
 
 func main() {
+	// Example for a copper pickaxe mining cobblestone
 	//set hotabar spaces that have copper pickaxe (inclusive)
-	const min = 1
-	const max = 9
+	const min = 1         //where the picaxe line starts
+	const max = 9         //where it should end
+	const sleepTime = 144 //how long it takes to break a picaxe or until it switches to the next
+	//If mining cobblestone with a cobblestone generator, remember to account for the time it takes to replace the cobblestone
 
 	currentSpace := min
 
@@ -22,7 +25,7 @@ func main() {
 
 	for currentSpace <= max {
 		fmt.Println("Waiting for picaxe to break")
-		time.Sleep(144 * time.Second) //~190 cobblestone (give or take for lag and such)
+		time.Sleep(sleepTime * time.Second) //~190 cobblestone (give or take for lag and such)
 		toMove := "num" + strconv.Itoa(currentSpace+1)
 		fmt.Println("moving to space: ", toMove)
 		robotgo.KeyTap(toMove)
