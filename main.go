aa1075c7059ea9f3489eaf9ccff8362037258740
Beijing-corn87 <111331826+bluekiwidev@users.noreply.github.com>
2025-11-22 20:51:49 +1300

    Implement code changes to enhance functionality and improve performance

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ import (
 func main() {
 	// Example for a copper pickaxe mining cobblestone
 	//set hotabar spaces that have copper pickaxe (inclusive)
-	const min = 1         //where the picaxe line starts
-	const max = 9         //where it should end
-	const sleepTime = 144 //how long it takes to break a picaxe or until it switches to the next
+	const min = 3         //where the picaxe line starts
+	const max = 7         //where it should end
+	const sleepTime = 370 //how long it takes to break a picaxe or until it switches to the next
 	//If mining cobblestone with a cobblestone generator, remember to account for the time it takes to replace the cobblestone
 	fmt.Println("Afker v1.0")
 	fmt.Println("Move your mouse to your posistion")
@@ -30,9 +30,13 @@ func main() {
 	for currentSpace <= max {
 		fmt.Println("Waiting for picaxe to break")
 		time.Sleep(sleepTime * time.Second) //~190 cobblestone (give or take for lag and such)
+		fmt.Println("Mousing up")
+		robotgo.MouseUp("left")
 		toMove := "num" + strconv.Itoa(currentSpace+1)
-		fmt.Println("moving to space: ", toMove)
+		fmt.Println("moving to space:", toMove)
 		robotgo.KeyTap(toMove)
+		robotgo.MouseDown("left")
+		fmt.Println("Mouse is down")
 		currentSpace++
 	}
 
